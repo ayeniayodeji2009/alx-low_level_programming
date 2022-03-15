1af612261ca13762c1aba7ee6d75edcaf291e064
ayeniayodeji2009 <ayeniayodeji2009@gmail.com>
2022-03-15 16:27:09 -0700

    Updating functions nested 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+
+
 /**
  *
  *  * main -  prints _putchar, followed by a new line
@@ -6,7 +9,19 @@
  *   * Return: Always 0 (Success)
  *
  *    */
-char main(void){
-_putchar("_putchar\n");
-return (0);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
+
+
