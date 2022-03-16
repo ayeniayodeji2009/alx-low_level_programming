883db94235b9b2d46535c3eb6815e294e53d66d7
ayeniayodeji2009 <ayeniayodeji2009@gmail.com>
2022-03-15 21:09:13 -0700

    Added function nested 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,15 +16,19 @@
 
 int print_sign(int n)
 {
-if(n > 0){
-return 1;
-_putchar("+");
-}else if(n == 0){
-return 0;
-_putchar("0");
-}else if(n < 0){
-return -1;
-_putchar("-");
+if (n > 0)
+{
+_putchar('+');
+return (1);
 }
-return 0;
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
 }
