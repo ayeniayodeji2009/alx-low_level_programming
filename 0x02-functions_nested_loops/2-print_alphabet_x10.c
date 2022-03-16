b73fa7aea340c0a0059c9b4df8b9de28cad2f340
ayeniayodeji2009 <ayeniayodeji2009@gmail.com>
2022-03-15 20:20:46 -0700

    Added function nested 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ *
+ *   * followed by a new line
+ *
+ *   
+ */
+void print_alphabet_x10(void)
+{
+int ch;
+int i;
+for(i = 0; i <= 10; i++){
+ch = "a";
+while(ch <= "z"){
+_putchar(ch);
+ch++;
+}
+_putchar("%d\n",i)
+}
+_putchar("\n");
+ return 0;
+}
