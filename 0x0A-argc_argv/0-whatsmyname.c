8a3705ba36bc62fbddd322610b9edce5fc1b54c4
ayeniayodeji2009 <48394569+ayeniayodeji2009@users.noreply.github.com>
2022-04-07 02:41:06 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-if(argc)
-printf("%s\n", argv[0]);
-return (0);
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
 }
