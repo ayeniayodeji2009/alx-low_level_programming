811fb2d9842714dcacc21a500aa9fb3a0722e2bb
ayeniayodeji2009 <48394569+ayeniayodeji2009@users.noreply.github.com>
2022-04-10 15:45:20 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,49 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
  *
- * Return: integer length of string
+ * Return: pointer to da ol'block nameen.
  */
-int _strlen(char *s)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i = 0;
+	char *p;
 
-	while (*s++)
-		i++;
-	return (i);
-}
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
 
-/**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: copy first n bytes of s2
- *
- * Return: void
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *s, *r;
-	unsigned int l1, l2;
-
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		s2 = "";
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-
-	l2 = l2 > n ? n : l2;
-	r = s = malloc(l1 + l2 + 1);
-	if (!s)
+	p = malloc(new_size);
+	if (!p)
 		return (NULL);
-	while (*s1)
-		*s++ = *s1++;
-	while (l2--)
-		*s++ = *s2++;
-	*s = '\0';
-	return (r);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
 }
